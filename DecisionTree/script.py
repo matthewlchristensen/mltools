307e092a1128da8b6106d22b7b9709dd164d682c
matthewlchristensen <mattswimftw@gmail.com>
2019-02-10 17:49:18 -0700

    Finished implementation. Added different strategies. Wrote testing script, w/ formatted strings for latex and console.

--- DecisionTree/script.py
+++ DecisionTree/script.py
@@ -19,7 +19,7 @@ def is_uniform(data_set: list):
 
 class DecisionTree:                                        # possible_values, num_indices, [unknown_mode = false],
     def __init__(self, training_data, max_depth, strategy, possible_values, num_indices,
-                 most_common="Nothing", depth=0, unknown_mode = False):
+                 most_common="Nothing", depth=0):
         """
         Takes in a list of examples and constructs a decision tree out of it.
         :param training_data:
@@ -87,8 +87,8 @@ class DecisionTree:                                        # possible_values, nu
         best_so_far = 0
         index_of_best_so_far = 0
         for i in range(len(possible_values)):
-            heuristic = strategy(i, self.data, possible_values, i in num_indices)
-            # heuristic = information_gain(i, self.data, possible_values, i in num_indices, strategy)
+            # heuristic = strategy(i, self.data, possible_values, i in num_indices)
+            heuristic = information_gain(i, self.data, possible_values, i in num_indices, strategy)
             if heuristic > best_so_far:
                 best_so_far = heuristic
                 index_of_best_so_far = i
@@ -111,10 +111,12 @@ def entropy(subset):
 
 
 def max_error(subset):
+    if not subset:
+        return 0
     label_set = list()
     for individual in subset:
         label_set.append(individual.label)
-    label_counter = Counter(label_list)
+    label_counter = Counter(label_set)
     most_common_count = label_counter.most_common(1)[0][1]
     total_count = len(subset)
     negatives = total_count - most_common_count
@@ -136,25 +138,21 @@ def gini(subset):
     return total_gini
 
 
-def information_gain(attribute, example_list: list, possible_values, is_numeric):
-# def information_gain(attribute, example_list: list, possible_values, is_numeric, strategy):
-    total_gain = entropy(example_list)
-    # total_gain = strategy(example_list)
+def information_gain(attribute, example_list: list, possible_values, is_numeric, strategy):
+    total_gain = strategy(example_list)
     if not is_numeric:
         partition = partition_categorical_data(example_list, attribute)
         for value in partition.keys():
             subset = partition[value]
             proportion = len(subset)/len(example_list)
-            total_gain -= proportion*entropy(subset)
-            # total_gain -= proportion*strategy(subset)
+            total_gain -= proportion*strategy(subset)
     else:
         partition = partition_numerical_data(example_list, attribute, possible_values[attribute])
         subset_above = partition["above"]
         proportion_above = len(subset_above)/len(example_list)
         subset_below = partition["below"]
         proportion_below = len(subset_below)/len(example_list)
-        total_gain -= proportion_above * entropy(subset_above) + proportion_below * entropy(subset_below)
-        # total_gain -= proportion_above * strategy(subset_above) + proportion_below * strategy(subset_below)
+        total_gain -= proportion_above * strategy(subset_above) + proportion_below * strategy(subset_below)
     return total_gain
 
 
@@ -189,6 +187,16 @@ def most_common_label(example_list: list):
         label_list.append(example.label)
     counter = Counter(label_list)
     return counter.most_common(1)[0][0]
+
+
+def most_common_attribute(example_list: list, attr):
+    attr_list = list()
+    for sample in example_list:
+        value = sample.attributes[attr]
+        if not value == "unknown":
+            attr_list.append(sample.attributes[attr])
+    count = Counter(attr_list)
+    return count.most_common(1)[0][0]
 
 
 def attr_values(example_list: list):
@@ -227,35 +235,145 @@ def is_num(string):
         return False
 
 
-if __name__ == "__main__":
-    examples = list()
-    print("hello world")
-    with open("bank/train.csv", 'r') as dataset:
+def load_data(file, unknown_replace=False):
+    result = list()
+    with open(file, 'r') as dataset:
         for line in dataset:
             data = line.strip().split(',')
             label = data[-1]
             attr = data[:-1]
-            examples.append(Example(attr, label))
-    tests = list()
-    with open("bank/test.csv", 'r') as test_set:
-        for line in test_set:
-            data = line.strip().split(',')
-            label = data[-1]
-            attr = data[:-1]
-            tests.append(Example(attr, label))
-    values, nums = attr_values(examples)
-    labels = most_common_label(examples)
-    print(values, nums)
-    print(labels)
+            result.append(Example(attr, label))
+    if not unknown_replace:
+        return result
+    most_commons = list()
+    attribute_length = len(result[0].attributes)
+    for i in range(attribute_length):
+        most_commons.append(most_common_attribute(result, i))
+    for sample in result:
+        for i in range(attribute_length):
+            value = sample.attributes[i]
+            if value == "unknown":
+                sample.attributes[i] = most_commons[i]
+    return result
 
-    label_list = list()
-    for example in examples:
-        label_list.append(example.label)
-    counter = Counter(label_list)
-    print(counter.most_common(1)[0][1])
-    # tree = DecisionTree(examples, 3, information_gain, values, nums)
-    # tree = DecisionTree(examples, 3, entropy, values, nums)
-    # tree = DecisionTree(examples, 3, max_error, values, nums)
-    # tree = DecisionTree(examples, 3, gini, values, nums)
-    # print(tree.test(examples))
-    # print(tree.test(tests))
+
+if __name__ == "__main__":
+    examples = list()
+    print("Hello Human\nI am a learning robot. Behold my training prowess:\n")
+
+    # initialize training and testing data
+    training_examples_car = load_data("car/train.csv")
+    training_examples_bank = load_data("bank/train.csv")
+    training_examples_bank_unknown = load_data("bank/train.csv", True)
+
+    testing_examples_car = load_data("car/test.csv")
+    testing_examples_bank = load_data("bank/test.csv")
+    testing_examples_bank_unknown = load_data("bank/test.csv", True)
+
+    car_val, car_num = attr_values(training_examples_car)
+    bank_val, bank_num = attr_values(training_examples_bank)
+    bank_val_u, bank_num_u = attr_values(training_examples_bank_unknown)
+
+    # initialize string formatting options
+    cat_format_string = "{:7s}{:>10s}{:>10s}{:>10s}\n"
+    num_format_string = "{:<7d}{:10.3f}{:10.3f}{:10.3f}\n"
+    cat_format_string_tex = "{:s}&{:s}&{:s}&{:s}\\\\\\hline\\hline\n"
+    num_format_string_tex = "{:d}&{:.3f}&{:.3f}&{:.3f}\\\\\\hline\n"
+
+    # Begin part 2 tests:----------------------------------------------------------------------------
+    car_train_err_results = cat_format_string.format("strat", "entropy", "gin", "max")
+    car_test_err_results = cat_format_string.format("strat", "entropy", "gin", "max")
+    car_train_err_results_tex = cat_format_string_tex.format("strat", "entropy", "gin", "max")
+    car_test_err_results_tex = cat_format_string_tex.format("strat", "entropy", "gin", "max")
+
+    for i in range(1, 7):
+        tree_ent = DecisionTree(training_examples_car, i, entropy, car_val, car_num)
+        tree_gin = DecisionTree(training_examples_car, i, gini, car_val, car_num)
+        tree_max = DecisionTree(training_examples_car, i, max_error, car_val, car_num)
+
+        ent_train_res = tree_ent.test(training_examples_car)
+        gin_train_res = tree_gin.test(training_examples_car)
+        max_train_res = tree_max.test(training_examples_car)
+        ent_test_res = tree_ent.test(testing_examples_car)
+        gin_test_res = tree_gin.test(testing_examples_car)
+        max_test_res = tree_max.test(testing_examples_car)
+
+        car_train_err_results += num_format_string.format(i, ent_train_res, gin_train_res, max_train_res)
+        car_test_err_results += num_format_string.format(i, ent_test_res, gin_test_res, max_test_res)
+        car_train_err_results_tex += num_format_string_tex.format(i, ent_train_res, gin_train_res, max_train_res)
+        car_test_err_results_tex += num_format_string_tex.format(i, ent_test_res, gin_test_res, max_test_res)
+
+    print("Part 2(b):\ncar training dataset, varying strategy (col) and depth(row):")
+    print(car_train_err_results)
+    # print(car_train_err_results_tex)
+    print("car testing dataset:")
+    print(car_test_err_results)
+    # print(car_test_err_results_tex)
+
+    # Part 3(a) tests:------------------------------------------------------------------------------------
+
+    print("Calculating Trees for bank data (this may take a minute)\n")
+
+    bank_train_err_results = cat_format_string.format("strat", "entropy", "gin", "max")
+    bank_test_err_results = cat_format_string.format("strat", "entropy", "gin", "max")
+    bank_train_err_results_tex = cat_format_string_tex.format("strat", "entropy", "gin", "max")
+    bank_test_err_results_tex = cat_format_string_tex.format("strat", "entropy", "gin", "max")
+
+    for i in range(1, 17):
+        tree_ent = DecisionTree(training_examples_bank, i, entropy, bank_val, bank_num)
+        tree_gin = DecisionTree(training_examples_bank, i, gini, bank_val, bank_num)
+        tree_max = DecisionTree(training_examples_bank, i, max_error, bank_val, bank_num)
+
+        ent_train_res = tree_ent.test(training_examples_bank)
+        gin_train_res = tree_gin.test(training_examples_bank)
+        max_train_res = tree_max.test(training_examples_bank)
+        ent_test_res = tree_ent.test(testing_examples_bank)
+        gin_test_res = tree_gin.test(testing_examples_bank)
+        max_test_res = tree_max.test(testing_examples_bank)
+
+        bank_train_err_results += num_format_string.format(i, ent_train_res, gin_train_res, max_train_res)
+        bank_test_err_results += num_format_string.format(i, ent_test_res, gin_test_res, max_test_res)
+        bank_train_err_results_tex += num_format_string_tex.format(i, ent_train_res, gin_train_res, max_train_res)
+        bank_test_err_results_tex += num_format_string_tex.format(i, ent_test_res, gin_test_res, max_test_res)
+
+    print("Part 3(b):\nbank training dataset:")
+    print(bank_train_err_results)
+    # print(bank_train_err_results_tex)
+    print("bank testing dataset:")
+    print(bank_test_err_results)
+    # print(bank_test_err_results_tex)
+
+    # Part 3(b):----------------------------------------------------------------------------------
+
+    print("Calculating Trees for bank data, replace unknown (this may take a minute)\n")
+
+    bank_train_err_results_unk = cat_format_string.format("strat", "entropy", "gin", "max")
+    bank_test_err_results_unk = cat_format_string.format("strat", "entropy", "gin", "max")
+    bank_train_err_results_unk_tex = cat_format_string_tex.format("strat", "entropy", "gin", "max")
+    bank_test_err_results_unk_tex = cat_format_string_tex.format("strat", "entropy", "gin", "max")
+
+    for i in range(1, 17):
+        tree_ent = DecisionTree(training_examples_bank_unknown, i, entropy, bank_val, bank_num)
+        tree_gin = DecisionTree(training_examples_bank_unknown, i, gini, bank_val, bank_num)
+        tree_max = DecisionTree(training_examples_bank_unknown, i, max_error, bank_val, bank_num)
+
+        ent_train_res = tree_ent.test(training_examples_bank)
+        gin_train_res = tree_gin.test(training_examples_bank)
+        max_train_res = tree_max.test(training_examples_bank)
+        ent_test_res = tree_ent.test(testing_examples_bank)
+        gin_test_res = tree_gin.test(testing_examples_bank)
+        max_test_res = tree_max.test(testing_examples_bank)
+
+        bank_train_err_results_unk += num_format_string.format(i, ent_train_res, gin_train_res, max_train_res)
+        bank_test_err_results_unk += num_format_string.format(i, ent_test_res, gin_test_res, max_test_res)
+        bank_train_err_results_unk_tex += num_format_string_tex.format(i, ent_train_res, gin_train_res, max_train_res)
+        bank_test_err_results_unk_tex += num_format_string_tex.format(i, ent_test_res, gin_test_res, max_test_res)
+
+    print("Part 3(c):\nbank training dataset, replace unknown:")
+    print(bank_train_err_results_unk)
+    # print(bank_train_err_results_unk_tex)
+    print("bank testing dataset, replace unknown:")
+    print(bank_test_err_results_unk)
+    # print(bank_test_err_results_unk_tex)
+
+    print("---------TESTS COMPLETE--------------")
